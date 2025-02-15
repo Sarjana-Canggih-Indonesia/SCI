df3a9f2e17b46135e0d8f0383de409535e788041
akidraham <saptadimahardhika@gmail.com>
2025-02-15 17:52:13 +0700

    [add] initial commit untuk halaman  aboutUs.php

---
+++ public_html/aboutUs.php
@@ -0,0 +1,314 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Tentang Kami - Penelitian Kita</title>
+    <!-- add icon link -->
+    <link rel="icon" href="images/logopkputih.png" type="image/x-icon" />
+    <!-- bikin icon -->
+    <!--CSS-->
+    <link rel="stylesheet" href="../css/styles.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <!--Font Awesome-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
+    <!-- Slick Slider css -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+    <!-- OWL CAROUSEL -->
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.css" />
+    <!-- ROBOTO -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+        rel="stylesheet" />
+    <style>
+        body {
+            font-family: "Roboto", sans-serif;
+        }
+
+        footer {
+            font-family: "Roboto", sans-serif;
+        }
+    </style>
+</head>
+
+<body>
+    <!--================Area Navigasi =================-->
+    <section class="navbar_area">
+        <nav class="navbar navbar-expand-lg fixed-top py-3">
+            <div class="container">
+                <a class="navbar-brand fw-bolder navbar-logo-pk" href="index.html">
+                    <img src="images/logopkcolor.png" alt="logo" style="width: 42px; height: 42px" /></a>
+                <button type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"
+                    class="navbar-toggler navbar-toggler-right">
+                    <i class="fa-solid fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a href="index.html" class="nav-link fw-bolder">Home </a>
+                        </li>
+                        <li class="nav-item active">
+                            <a href="tentangkami.html" class="nav-link fw-bolder">Tentang Kami
+                            </a>
+                        </li>
+                        <!--DROPDOWN ASISTENSI PENELITIAN-->
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle fw-bolder" href="#" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                Asistensi Penelitian
+                            </a>
+                            <ul class="dropdown-menu" style="background-color: #fff">
+                                <li>
+                                    <a class="dropdown-item" href="asistensi/penelitian/skripsi.html">Skripsi</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="asistensi/penelitian/tesis.html">Tesis</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="asistensi/penelitian/disertasi.html">Disertasi</a>
+                                </li>
+                                <hr class="dropdown-divider" style="border-color: black" />
+                                <li>
+                                    <a class="dropdown-item" href="asistensi/penelitian/jurnal.html">Jurnal</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <!--end of DROPDOWN ASISTENSI PENELITIAN-->
+                        <!--DROPDOWN ASISTENSI PENULISAN-->
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle fw-bolder" href="#" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                Asistensi Penulisan
+                            </a>
+                            <ul class="dropdown-menu" style="background-color: #fff">
+                                <li>
+                                    <a class="dropdown-item"
+                                        href="asistensi/penulisan/penulisan.html#asistensipenulisan">Artikel</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item"
+                                        href="asistensi/penulisan/penulisan.html#asistensipenulisan">Proofreading</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item"
+                                        href="asistensi/penulisan/penulisan.html#asistensipenulisan">Edting Naskah
+                                        Buku</a>
+                                </li>
+                                <hr class="dropdown-divider" style="border-color: black" />
+                                <li>
+                                    <a class="dropdown-item"
+                                        href="asistensi/penulisan/penulisan.html#asistensipenulisan">Translate</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <!--end of DROPDOWN ASISTENSI PENULISAN-->
+                        <li class="nav-item">
+                            <a href="promo/promo.html" class="nav-link fw-bolder">Promo</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="karir.html" class="nav-link fw-bolder">Karir</a>
+                        </li>
+                    </ul>
+                    <div class="d-flex ms-auto">
+                        <a class="nav-link ms-2 fw-bolder px-lg-2" href="https://sarjana-canggih-indonesia.myr.id">Pesan
+                            Sekarang</a>
+                        <a class="nav-link ms-2 fw-bolder px-lg-2" href="https://linktr.ee/sarjanacanggih">Kontak
+                            Kami</a>
+                        <a class="nav-link ms-2 fw-bolder px-lg-2" href="#">Sarjana Canggih</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </section>
+    <!--================Akhir Area Navigasi=================-->
+    <!--================Area Jumbotron=================-->
+    <section class="jumbotron-pk tentang-kami">
+        <div class="container py-4">
+            <div class="jumbotron bg p-5 mb-4 rounded-3">
+                <div class="container-fluid py-5 tk_text">
+                    <h1 class="display-5 fw-bold">Tentang</h1>
+                    <h1 class="display-5 fw-bold">Kami</h1>
+                    <p class="col-md-8 fs-4"></p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--================Akhir Area Jumbotron=================-->
+    <br />
+    <br />
+    <!--================Area Kata Mereka=================-->
+    <section class="katamereka">
+        <div class="container">
+            <div class="row">
+                <p class="fs-1 fw-bold">Kata mereka "Joki"? Kami jawab:</p>
+                <p class="fs-5" style="text-align: justify">
+                    "Kami tidak berbeda dengan jasa pihak ketiga lainnya yang menjadikan
+                    ide-ide pelanggan kami menjadi karya nyata. Pengalaman kami
+                    menunjukkan bahwa jasa kami tidak serta merta memberikan kuasa bagi
+                    siapapun untuk mencapai apa yang tidak mereka mampu. Kami mengerti
+                    sulitnya memahami konsep penelitian. Karena itu, melalui jasa
+                    asistensi, kami membantu klien mempercepat proses belajar penelitian
+                    dengan melakukannya dan mendampingi setiap prosesnya."
+                </p>
+            </div>
+        </div>
+    </section>
+    <!--================End of Area Kata Mereka=================-->
+    <!--================Area VISI MISI=================-->
+    <section class="tentangkami py-5">
+        <div class="container tk-text">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h4 class="fs-1 text-center fw-bold">
+                        VISI <font color="#FFC300">KAMI</font>
+                    </h4>
+                    <p class="fs-2 text-center">
+                        "Mendukung percepatan dan peningkatan kualitas penelitian di
+                        Indonesia."
+                    </p>
+                </div>
+                <div class="col-lg-12 py-5">
+                    <h4 class="fs-1 text-center fw-bold">
+                        MISI <font color="#FFC300">KAMI</font>
+                    </h4>
+                    <ol>
+                        <li class="fs-5">
+                            Mendukung pemerataan dengan mendampingi mahasiswa yang memiliki
+                            waktu yang terbatas.
+                        </li>
+                        <li class="fs-5">
+                            Mendorong ide-ide brilian agar dapat tersampaikan secara
+                            analitis dan kritis melalui berbagai bentuk penelitian.
+                        </li>
+                        <li class="fs-5">
+                            Memandu mahasiswa Indonesia mengenai cara melakukan penelitian
+                            yang terstruktur dan sistematis.
+                        </li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--================Akhir Area VISI MISI=================-->
+    <!--================Area Our Values=================-->
+    <section class="ourvalues">
+        <div class="container">
+            <div class="row">
+                <img src="images/tentangkami/Our Values.webp" alt="" />
+            </div>
+        </div>
+    </section>
+    <!--================Akhir Area Our Values=================-->
+    <br /><br /><br />
+</body>
+<!--================ Area Footer =================-->
+<footer class="footer footer_pk">
+    <div class="container">
+        <div class="row">
+            <!-- Logo and Company Name -->
+            <div class="col-md-3 footer-logo">
+                <img src="images/logopkputih.png" alt="" />
+            </div>
+            <!-- Info Kontak -->
+            <div class="col-md-3 footer-info">
+                <h5>Hubungi Kami</h5>
+                <p>Alamat:</p>
+                <p>Kecamatan Singosari, Kabupaten Malang, 65153</p>
+                <p>Telepon:</p>
+                <p>+62815-8500-0640</p>
+            </div>
+            <!-- Layanan-->
+            <div class="col-md-3 footer-info">
+                <h5>Layanan Kami</h5>
+                <h5 class="py-2">Asistensi Penelitian</h5>
+                <p>
+                    <a href="asistensi/penelitian/skripsi.html" class="footernodecor">Skripsi</a>
+                </p>
+                <p>
+                    <a href="asistensi/penelitian/tesis.html" class="footernodecor">Tesis</a>
+                </p>
+                <p>
+                    <a href="asistensi/penelitian/disertasi.html" class="footernodecor">Disertasi</a>
+                </p>
+                <p>
+                    <a href="asistensi/penelitian/jurnal.html" class="footernodecor">Jurnal</a>
+                </p>
+                <h5 class="py-2">Asistensi Penulisan</h5>
+                <p>
+                    <a href="asistensi/penulisan/penulisan.html#asistensipenulisan" class="footernodecor">Makalah /
+                        Artikel</a>
+                </p>
+                <p>
+                    <a href="asistensi/penulisan/penulisan.html#asistensipenulisan"
+                        class="footernodecor">Proofreading</a>
+                </p>
+                <p>
+                    <a href="asistensi/penulisan/penulisan.html#asistensipenulisan" class="footernodecor">Editing
+                        Nashkah Buku</a>
+                </p>
+                <p>
+                    <a href="asistensi/penulisan/penulisan.html#asistensipenulisan" class="footernodecor">Translate</a>
+                </p>
+            </div>
+            <!--End of Layanan-->
+            <!-- Social Media -->
+            <div class="col-md-3 footer-social">
+                <h5>Ikuti Kami</h5>
+                <a href="https://facebook.com/PenelitianKita" target="_blank">
+                    <i class="social-icons fab fa-facebook-f"></i><span class="social-text">Facebook</span>
+                </a>
+                <a href="https://www.instagram.com/penelitian_kita/" target="_blank">
+                    <i class="social-icons fab fa-instagram"></i><span class="social-text">Instagram</span>
+                </a>
+                <a href="https://wa.me/+6281585000640" target="_blank">
+                    <i class="social-icons fab fa-whatsapp"></i><span class="social-text">Whatsapp</span>
+                </a>
+                <a href="#" target="_blank">
+                    <i class="social-icons fab fa-youtube"></i><span class="social-text">Youtube</span>
+                </a>
+                <a href="#" target="_blank">
+                    <i class="social-icons fab fa-linkedin-in"></i><span class="social-text">Linkedin</span>
+                </a>
+            </div>
+        </div>
+        <hr />
+        <!-- Navigasi Links -->
+        <div class="row mt-3">
+            <div class="col-md-12 text-center footer-nav">
+                <a href="index.html">Home</a>
+                <a href="tentangkami.html">Tentang Kami</a>
+                <a href="https://linktr.ee/sarjanacanggih">Kontak Kami</a>
+                <a href="#">FAQ</a>
+            </div>
+        </div>
+        <!-- Copyright -->
+        <div class="row mt-3">
+            <div class="col-lg-6 offset-lg-3 text-center text-white-50">
+                <p>
+                    Copyright &copy; 2024 &middot; Sarjana Canggih Indonesia<a href="#">&nbsp;</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
+<!--================ End of Area Footer =================-->
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+<!-- Slick Slider JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/owl.carousel.js"></script>
+<script src="../js/custom.js"></script>
+
+</html>
